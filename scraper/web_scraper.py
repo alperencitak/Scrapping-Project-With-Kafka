73396f4ffffe6e4c9057b7b0d5b45ac0248b8291
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-04-29 11:14:58 +0300

    Web Scraper updated to scrap topics every 1 second

--- scraper/web_scraper.py
+++ scraper/web_scraper.py
@@ -7,27 +7,25 @@ producer = KafkaProducer(bootstrap_servers='kafka-server:9092', value_serializer
 
 def scrape_page():
     try:
-        response = requests.get("https://scrapeme.live/shop/")
-        soup = BeautifulSoup(response.text, 'html.parser')
+        while True:
+            response = requests.get("https://scrapeme.live/shop/")
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-        products = []
-        for product in soup.select('li.product'):
-            data = {
-                "name": product.find('h2').text.strip(),
-                "price": product.find('span', class_='price').text.strip(),
-                "image": product.find('img')['src']
-            }
-            products.append(data)
-            producer.send('scraped_products', value=data)
-            print(f"Sent: {data}")
+            for product in soup.select('li.product'):
+                data = {
+                    "name": product.find('h2').text.strip(),
+                    "price": product.find('span', class_='price').text.strip(),
+                    "stock": product.find('p', class_='stock').text.strip() if product.find('p', class_='stock') else "Stok Yok",
+                    "image": product.find('img')['src']
+                }
 
-        return products
+                producer.send('scraped_products', value=data)
+                time.sleep(1)
+
     except Exception as e:
-        print(f"Hata oluştu: {str(e)}")
-        return []
+        print(f"Error: {str(e)}")
     finally:
         producer.flush()
-        producer.close()
 
 
 if __name__ == "__main__":
